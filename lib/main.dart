01645ab5c92bf469db4f3ce1ccd19d8096c2d780
saci zakaria <sacideveloppeur@gmail.com>
2021-10-19 16:14:07 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,120 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:images_picker/images_picker.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String? path;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  getImage() async{
-
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("109. Image Picker"),
-      ),
-      body: const Center(
-        child: Text("No Image selected"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add_a_photo),
-        onPressed: () {
-          var ad = AlertDialog(
-            title: Text("Chose Picture From "),
-            content: Container(
-              height: 200,
-              child: Column(
-                children: [
-                  Divider(
-                    color: Colors.black,
-                    thickness: 3,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Column(
+          children: [
+            ElevatedButton(
+              child: Text('pick'),
+              onPressed: () async {
+                List<Media>? res = await ImagesPicker.pick(
+                  count: 3,
+                  pickType: PickType.all,
+                  language: Language.System,
+                  // maxSize: 500,
+                  cropOpt: CropOption(
+                    aspectRatio: CropAspectRatio.wh16x9,
                   ),
-                  Container(
-                    // width: 250,
-                    color: Colors.indigo,
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.image,
-                        color: Colors.white,
-                        size: 40,
-                      ),
-                      title: Text("Gallery"),
-                      onTap: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Container(
-                    // width: 250,
-                    color: Colors.indigo,
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.add_a_photo,
-                        color: Colors.white,
-                        size: 40,
-                      ),
-                      title: Text("Camera"),
-                      onTap: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                ],
-              ),
+                );
+                if (res != null) {
+                  print(res.map((e) => e.path).toList());
+                  setState(() {
+                    path = res[0].thumbPath;
+                  });
+                  // bool status = await ImagesPicker.saveImageToAlbum(File(res[0]?.path));
+                  // print(status);
+                }
+              },
             ),
-          );
-          showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return ad;
-              });
-        },
+            ElevatedButton(
+              child: Text('openCamera'),
+              onPressed: () async {
+                List<Media>? res = await ImagesPicker.openCamera(
+                  pickType: PickType.image,
+                  quality: 0.5,
+                  // cropOpt: CropOption(
+                  //   aspectRatio: CropAspectRatio.wh16x9,
+                  // ),
+                  // maxTime: 60,
+                );
+                if (res != null) {
+                  print(res[0].path);
+                  setState(() {
+                    path = res[0].thumbPath;
+                  });
+                }
+              },
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                File file =
+                await downloadFile('https://cdn.chavesgu.com/logo.png');
+                bool res = await ImagesPicker.saveImageToAlbum(file,
+                    albumName: "chaves");
+                print(res);
+              },
+              child: Text('saveNetworkImageToAlbum'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                File file = await downloadFile(
+                    'https://cdn.chavesgu.com/SampleVideo.mp4');
+                bool res = await ImagesPicker.saveVideoToAlbum(file,
+                    albumName: "chaves");
+                print(res);
+              },
+              child: Text('saveNetworkVideoToAlbum'),
+            ),
+            path != null
+                ? Container(
+              height: 200,
+              child: Image.file(
+                File(path!),
+                fit: BoxFit.contain,
+              ),
+            )
+                : SizedBox.shrink(),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<File> downloadFile(String url) async {
+    Dio simple = Dio();
+    String savePath = Directory.systemTemp.path + '/' + url.split('/').last;
+    await simple.download(url, savePath,
+        options: Options(responseType: ResponseType.bytes));
+    print(savePath);
+    File file = new File(savePath);
+    return file;
   }
 }
